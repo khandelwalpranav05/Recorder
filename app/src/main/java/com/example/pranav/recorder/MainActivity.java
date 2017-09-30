9cdf7c49f1d6e6714b3856dd95f4da3f628e35f7
khandelwalpranav05 <khandelwalpranav@rocketmail.com>
2017-10-01 02:31:29 +0530

    Initial commit

---
+++ app/src/main/java/com/example/pranav/recorder/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.pranav.recorder;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.astuetz.PagerSlidingTabStrip;
+
+public class MainActivity extends AppCompatActivity {
+
+    private PagerSlidingTabStrip tabs;
+    private ViewPager pager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+        pager = (ViewPager) findViewById(R.id.pager);
+
+        
+    }
+}
